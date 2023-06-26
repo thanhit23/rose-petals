ee12364019f38b82db7ab0c826006b68e2f52fee
Duy Thanh <duythanhit203@gmail.com>
2023-06-26 10:51:23 +0700

    [Bugfix] fix master

--- src/containers/Authenticated/types.ts
+++ src/containers/Authenticated/types.ts
@@ -1,21 +1,8 @@
 export interface Props {
   auth: object;
   children: JSX.Element;
-  onSendRequestToken: (token: string) => void;
-  notToken: () => void;
 }
 
 export interface States {
   global: { auth: object };
-}
-
-export interface PropsSendReqToken {
-  type: string;
-  payload: {
-    token: string;
-  };
-}
-export interface ApiResponse {
-  data: object;
-  status: number;
 }
