f897c37d0f879c3984fac02b125a5a7e55625ab4
Starusss <thanhhien.study@gmail.com>
2023-09-28 20:29:35 +0700

    [UI] change filter search product

--- src/components/FilterPanel/index.tsx
+++ src/components/FilterPanel/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-key */
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import Divider from '@mui/material/Divider';
@@ -11,51 +11,14 @@ import Paper from '@mui/material/Paper';
 import Rating from '@mui/material/Rating';
 import TextField from '@mui/material/TextField';
 
+import { Props } from '../ListBrand/types';
 import messages from './messages';
 import styles from './styles';
 
-function FilterPanel() {
-  const [openDropdown, setOpenDropdown] = useState(false);
-  const handleToggleDropdown = () => setOpenDropdown(!openDropdown);
-
+function FilterPanel({ listBrand }: Props) {
   return (
     <Grid item md={3}>
       <Paper sx={styles.wrapPaper}>
-        <Box component="h6" sx={styles.boxTitleCategory}>
-          <FormattedMessage {...messages.categories} />
-        </Box>
-        <Box
-          sx={{
-            ...styles.wrapDropdownCategoryItem,
-            height: () => (openDropdown ? '143px' : '33px'),
-          }}
-        >
-          <Box sx={styles.boxDropdownCategoryItem} onClick={handleToggleDropdown}>
-            <Box component="span" sx={styles.boxBathPreparations}>
-              Bath Preparations
-            </Box>
-            <ChevronRightIcon fontSize="small" />
-          </Box>
-          <Box component="p" sx={styles.boxBathPreparationsItem}>
-            Bubble Bath
-          </Box>
-          <Box component="p" sx={styles.boxBathPreparationsItem}>
-            Bath Capsules
-          </Box>
-          <Box component="p" sx={styles.boxBathPreparationsItem}>
-            Others
-          </Box>
-        </Box>
-        <Box component="p" sx={styles.boxCategoryItem}>
-          Eye Makeup Preparations
-        </Box>
-        <Box component="p" sx={styles.boxCategoryItem}>
-          Fragrance
-        </Box>
-        <Box component="p" sx={styles.boxCategoryItem}>
-          Hair Preparations
-        </Box>
-        <Divider sx={styles.divider} />
         <Box component="h6" sx={styles.boxPriceRange}>
           <FormattedMessage {...messages.priceRange} />
         </Box>
@@ -70,48 +33,14 @@ function FilterPanel() {
         <Box component="h6" sx={styles.boxPriceRange}>
           <FormattedMessage {...messages.brands} />
         </Box>
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>Macc</Box>}
-        />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>Karts</Box>}
-        />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>Baals</Box>}
-        />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>Bukks</Box>}
-        />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>Luasis</Box>}
-        />
-        <Divider sx={styles.dividerTwo} />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>On Sale</Box>}
-        />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>In Stock</Box>}
-        />
-        <FormControlLabel
-          sx={styles.flex}
-          control={<Checkbox color="default" size="small" />}
-          label={<Box sx={styles.labelFormControl}>Featured</Box>}
-        />
-        <Divider sx={styles.dividerTwo} />
+        {listBrand.map(item => (
+          <FormControlLabel
+            sx={styles.flex}
+            control={<Checkbox color="default" size="small" />}
+            label={<Box sx={styles.labelFormControl}>{item.name}</Box>}
+          />
+        ))}
+
         <Box component="h6" sx={styles.boxPriceRange}>
           <FormattedMessage {...messages.ratings} />
         </Box>
@@ -141,17 +70,6 @@ function FilterPanel() {
           label={<Rating name="read-only" value={1} readOnly size="small" />}
         />
         <Divider sx={styles.dividerTwo} />
-        <Box component="h6" sx={styles.boxPriceRange}>
-          Colors
-        </Box>
-        <Box sx={styles.wrapColors}>
-          <Box sx={{ ...styles.colors, ...styles.bg_black }} />
-          <Box sx={{ ...styles.colors, ...styles.bg_pink }} />
-          <Box sx={{ ...styles.colors, ...styles.bg_yellow }} />
-          <Box sx={{ ...styles.colors, ...styles.bg_light_green }} />
-          <Box sx={{ ...styles.colors, ...styles.bg_blue }} />
-          <Box sx={{ ...styles.colors, ...styles.bg_purple_blood }} />
-        </Box>
       </Paper>
     </Grid>
   );
--- src/containers/ProductSearch/index.tsx
+++ src/containers/ProductSearch/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import { useQuery } from '@tanstack/react-query';
 
 import FilterPanel from 'src/components/FilterPanel';
 import Pagination from 'src/components/Pagination';
@@ -11,8 +12,16 @@ import styles from 'src/components/ProductItem/styles';
 import ProductViewInList from 'src/components/ProductViewInList';
 import SortBar from 'src/components/SortBar';
 
+import { getBrands } from '../HomePage/httpClients';
+
 function ProductSearch() {
   const [viewList, setViewList] = useState(false);
+  const { data: listBrand = [] } = useQuery({
+    queryKey: ['getBrands'],
+    queryFn: () => getBrands(),
+    retry: 0,
+    select: ({ data: { data } }) => data,
+  });
   const handleProductSearch = () => {
     // eslint-disable-next-line no-console
     console.log('asdasdasd');
@@ -48,7 +57,7 @@ function ProductSearch() {
     <Container maxWidth="lg" sx={{ margin: '32px auto' }}>
       <SortBar changeView={handleChangeView} viewList={viewList} />
       <Grid container spacing={{ xs: 3 }}>
-        <FilterPanel />
+        <FilterPanel listBrand={listBrand} />
         <Grid item xs={12} md={9}>
           {renderProduct()}
           <Box sx={styles.boxQuantityPaginationProduct}>
