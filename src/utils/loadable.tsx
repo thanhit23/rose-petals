e588530983bc9f315ae2a35e5757111b388e18f6
Duy Thanh <duythanhit203@gmail.com>
2023-10-13 20:22:52 +0700

    [Feature] config run eslint pre-commit

--- src/utils/loadable.tsx
+++ src/utils/loadable.tsx
@@ -3,6 +3,7 @@ import React, { Suspense, lazy } from 'react';
 const loadable = (importFunc: any, { fallback = null }: { fallback: any }) => {
   const LazyComponent = lazy(importFunc);
 
+  // eslint-disable-next-line react/display-name
   return (props: any) => (
     <Suspense fallback={fallback}>
       <LazyComponent {...props} />
