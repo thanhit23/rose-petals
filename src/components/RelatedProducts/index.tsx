7a69030ea9346f88545b7e08a9140bc13496b160
Hoang Quang Huy <hoanghuy02062002@gmail.com>
2023-10-22 17:04:47 +0700

    [Feature] show related products

--- src/components/RelatedProducts/index.tsx
+++ src/components/RelatedProducts/index.tsx
@@ -14,10 +14,17 @@ import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Rating from '@mui/material/Rating';
 
+import { Product } from 'src/common/types';
+import { PATH_PUBLIC } from 'src/routes/paths';
+
 import messages from './messages';
 import styles from './styles';
 
-function RelatedProducts() {
+type Props = {
+  listRelatedProduct: Product[];
+};
+
+const RelatedProducts: React.FC<Props> = ({ listRelatedProduct }) => {
   const [quantity, setQuantity] = useState(0);
 
   const handleIncrease = () => {
@@ -32,241 +39,78 @@ function RelatedProducts() {
 
   return (
     <Box marginBottom="60px">
-      <Box component="h3">
+      <Box component="h3" sx={styles.relatedTitle}>
         <FormattedMessage {...messages.title} />
       </Box>
       <Grid container spacing={{ xs: 8 }}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper sx={styles.paperProduct}>
-            <Box sx={styles.boxImage}>
-              <Chip label="12% off" sx={styles.chipSale} />
-              <Box sx={styles.boxIcon} className="box--icon">
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <VisibilityIcon fontSize="small" />
-                </IconButton>
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <FavoriteIcon fontSize="small" />
-                </IconButton>
+        {listRelatedProduct.map(productRelate => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={productRelate._id}>
+            <Paper sx={styles.paperProduct}>
+              <Box sx={styles.boxImage}>
+                <Chip label="12% off" sx={styles.chipSale} />
+                <Box sx={styles.boxIcon} className="box--icon">
+                  <IconButton aria-label="delete" size="medium" sx={styles.icon}>
+                    <VisibilityIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton aria-label="delete" size="medium" sx={styles.icon}>
+                    <FavoriteIcon fontSize="small" />
+                  </IconButton>
+                </Box>
+                <Link to={PATH_PUBLIC.product.slug(productRelate?.slug, productRelate?._id)}>
+                  <Box
+                    component="img"
+                    src={productRelate.thumbnail}
+                    alt={productRelate.name}
+                    sx={styles.imageProduct}
+                  />
+                </Link>
               </Box>
-              <Link to="/">
-                <Box
-                  component="img"
-                  src="https://bazar-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FGroceries%2F8.ColgateAdvanceProtectionToothpaste.png&w=3840&q=75"
-                  sx={styles.imageProduct}
-                />
-              </Link>
-            </Box>
-            <Box padding="1rem">
-              <Box display="flex">
-                <Box sx={styles.informationProduct}>
-                  <Link to="/">
-                    <Box component="h3" sx={styles.nameProduct}>
-                      Colgate Advance Protection Toothpaste
-                    </Box>
-                  </Link>
-                  <Rating name="read-only" value={3} readOnly sx={{ fontSize: '1.25rem' }} />
-                  <Box sx={styles.priceProduct}>
-                    <Box fontWeight={600} color="#D23F57">
-                      $220.00
-                    </Box>
-                    <Box fontWeight={600} color="#7D879C">
-                      <del>$220.00</del>
+              <Box padding="1rem">
+                <Box display="flex">
+                  <Box sx={styles.informationProduct}>
+                    <Link to={PATH_PUBLIC.product.slug(productRelate?.slug, productRelate?._id)}>
+                      <Box component="h3" sx={styles.nameProduct}>
+                        {productRelate.name}
+                      </Box>
+                    </Link>
+                    <Rating
+                      name="read-only"
+                      value={Number(productRelate.rating)}
+                      readOnly
+                      sx={{ fontSize: '1.25rem' }}
+                    />
+                    <Box sx={styles.priceProduct}>
+                      <Box fontWeight={600} color="#D23F57">
+                        {productRelate.price}
+                      </Box>
+                      <Box fontWeight={600} color="#7D879C">
+                        <del>{productRelate.price}</del>
+                      </Box>
                     </Box>
                   </Box>
-                </Box>
-                <Box sx={styles.boxAddToCart}>
-                  <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleIncrease}>
-                    <AddIcon fontSize="small" />
-                  </Button>
-                  {quantity ? (
-                    <>
-                      <Box fontWeight={600} color="rgb(43, 52, 69)">
-                        {quantity}
-                      </Box>
-                      <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleReduce}>
-                        <RemoveIcon fontSize="small" />
-                      </Button>
-                    </>
-                  ) : null}
-                </Box>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper sx={styles.paperProduct}>
-            <Box sx={styles.boxImage}>
-              <Chip label="12% off" sx={styles.chipSale} />
-              <Box sx={styles.boxIcon} className="box--icon">
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <VisibilityIcon fontSize="small" />
-                </IconButton>
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <FavoriteIcon fontSize="small" />
-                </IconButton>
-              </Box>
-              <Link to="/">
-                <Box
-                  component="img"
-                  src="https://bazar-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FGroceries%2F8.ColgateAdvanceProtectionToothpaste.png&w=3840&q=75"
-                  sx={styles.imageProduct}
-                />
-              </Link>
-            </Box>
-            <Box padding="1rem">
-              <Box display="flex">
-                <Box sx={styles.informationProduct}>
-                  <Link to="/">
-                    <Box component="h3" sx={styles.nameProduct}>
-                      Colgate Advance Protection Toothpaste
-                    </Box>
-                  </Link>
-                  <Rating name="read-only" value={3} readOnly sx={{ fontSize: '1.25rem' }} />
-                  <Box sx={styles.priceProduct}>
-                    <Box fontWeight={600} color="#D23F57">
-                      $220.00
-                    </Box>
-                    <Box fontWeight={600} color="#7D879C">
-                      <del>$220.00</del>
-                    </Box>
+                  <Box sx={styles.boxAddToCart}>
+                    <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleIncrease}>
+                      <AddIcon fontSize="small" />
+                    </Button>
+                    {quantity ? (
+                      <>
+                        <Box fontWeight={600} color="rgb(43, 52, 69)">
+                          {quantity}
+                        </Box>
+                        <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleReduce}>
+                          <RemoveIcon fontSize="small" />
+                        </Button>
+                      </>
+                    ) : null}
                   </Box>
                 </Box>
-                <Box sx={styles.boxAddToCart}>
-                  <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleIncrease}>
-                    <AddIcon fontSize="small" />
-                  </Button>
-                  {quantity ? (
-                    <>
-                      <Box fontWeight={600} color="rgb(43, 52, 69)">
-                        {quantity}
-                      </Box>
-                      <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleReduce}>
-                        <RemoveIcon fontSize="small" />
-                      </Button>
-                    </>
-                  ) : null}
-                </Box>
               </Box>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper sx={styles.paperProduct}>
-            <Box sx={styles.boxImage}>
-              <Chip label="12% off" sx={styles.chipSale} />
-              <Box sx={styles.boxIcon} className="box--icon">
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <VisibilityIcon fontSize="small" />
-                </IconButton>
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <FavoriteIcon fontSize="small" />
-                </IconButton>
-              </Box>
-              <Link to="/">
-                <Box
-                  component="img"
-                  src="https://bazar-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FGroceries%2F8.ColgateAdvanceProtectionToothpaste.png&w=3840&q=75"
-                  sx={styles.imageProduct}
-                />
-              </Link>
-            </Box>
-            <Box padding="1rem">
-              <Box display="flex">
-                <Box sx={styles.informationProduct}>
-                  <Link to="/">
-                    <Box component="h3" sx={styles.nameProduct}>
-                      Colgate Advance Protection Toothpaste
-                    </Box>
-                  </Link>
-                  <Rating name="read-only" value={3} readOnly sx={{ fontSize: '1.25rem' }} />
-                  <Box sx={styles.priceProduct}>
-                    <Box fontWeight={600} color="#D23F57">
-                      $220.00
-                    </Box>
-                    <Box fontWeight={600} color="#7D879C">
-                      <del>$220.00</del>
-                    </Box>
-                  </Box>
-                </Box>
-                <Box sx={styles.boxAddToCart}>
-                  <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleIncrease}>
-                    <AddIcon fontSize="small" />
-                  </Button>
-                  {quantity ? (
-                    <>
-                      <Box fontWeight={600} color="rgb(43, 52, 69)">
-                        {quantity}
-                      </Box>
-                      <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleReduce}>
-                        <RemoveIcon fontSize="small" />
-                      </Button>
-                    </>
-                  ) : null}
-                </Box>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Paper sx={styles.paperProduct}>
-            <Box sx={styles.boxImage}>
-              <Chip label="12% off" sx={styles.chipSale} />
-              <Box sx={styles.boxIcon} className="box--icon">
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <VisibilityIcon fontSize="small" />
-                </IconButton>
-                <IconButton aria-label="delete" size="medium" sx={styles.icon}>
-                  <FavoriteIcon fontSize="small" />
-                </IconButton>
-              </Box>
-              <Link to="/">
-                <Box
-                  component="img"
-                  src="https://bazar-react.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2FGroceries%2F8.ColgateAdvanceProtectionToothpaste.png&w=3840&q=75"
-                  sx={styles.imageProduct}
-                />
-              </Link>
-            </Box>
-            <Box padding="1rem">
-              <Box display="flex">
-                <Box sx={styles.informationProduct}>
-                  <Link to="/">
-                    <Box component="h3" sx={styles.nameProduct}>
-                      Colgate Advance Protection Toothpaste
-                    </Box>
-                  </Link>
-                  <Rating name="read-only" value={3} readOnly sx={{ fontSize: '1.25rem' }} />
-                  <Box sx={styles.priceProduct}>
-                    <Box fontWeight={600} color="#D23F57">
-                      $220.00
-                    </Box>
-                    <Box fontWeight={600} color="#7D879C">
-                      <del>$220.00</del>
-                    </Box>
-                  </Box>
-                </Box>
-                <Box sx={styles.boxAddToCart}>
-                  <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleIncrease}>
-                    <AddIcon fontSize="small" />
-                  </Button>
-                  {quantity ? (
-                    <>
-                      <Box fontWeight={600} color="rgb(43, 52, 69)">
-                        {quantity}
-                      </Box>
-                      <Button variant="outlined" sx={styles.btnAddToCart} onClick={handleReduce}>
-                        <RemoveIcon fontSize="small" />
-                      </Button>
-                    </>
-                  ) : null}
-                </Box>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
+            </Paper>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
-}
+};
 
 export default RelatedProducts;
--- src/containers/ProductDetail/index.tsx
+++ src/containers/ProductDetail/index.tsx
@@ -2,11 +2,14 @@ import { createContext } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import Container from '@mui/material/Container';
+import { useQuery } from '@tanstack/react-query';
 
 import DetailReviewTabbedPane from 'src/components/DetailReviewTabbedPane';
 import ProductBriefing from 'src/components/ProductBriefing';
 import RelatedProducts from 'src/components/RelatedProducts';
 import { useGetProductDetail } from 'src/queries/product';
+
+import { getRelatedProducts } from './services';
 
 export const DescriptionContext = createContext('');
 
@@ -21,13 +24,20 @@ function ProductDetail() {
     console.log(data);
   };
 
+  const { data: listRelatedProducts = [] } = useQuery({
+    queryKey: ['RelatedProducts', data?.category.id],
+    queryFn: () => getRelatedProducts(data?.category.id as string),
+    enabled: !!data?.category.id,
+    select: ({ data: { data } }) => data,
+  });
+
   return (
     <Container maxWidth="lg" sx={{ margin: '32px auto' }}>
       <ProductBriefing product={data} />
       <DescriptionContext.Provider value={data?.description || ''}>
         <DetailReviewTabbedPane handleSubmitReview={handleSubmitReview} />
       </DescriptionContext.Provider>
-      <RelatedProducts />
+      <RelatedProducts listRelatedProduct={listRelatedProducts} />
     </Container>
   );
 }
