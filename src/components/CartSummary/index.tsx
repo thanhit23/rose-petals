e97da66a291fdca14ad4b833e9c6d70f7a6b74b8
Starusss <thanhhien.study@gmail.com>
2023-11-18 11:24:31 +0700

    [UI] Remove field comment in cart

--- src/components/CartSummary/index.tsx
+++ src/components/CartSummary/index.tsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
-import FormControl from '@mui/material/FormControl';
 import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
 
 import { Props } from 'src/containers/Cart/types';
 import formatPrice from 'src/helpers/formatPrice';
@@ -32,17 +30,6 @@ const CartSummary: React.FC<Props> = ({ productList }) => {
         </Box>
       </Box>
       <Divider sx={styles.divider} />
-      <Box sx={styles.boxTitleComment}>
-        <Box component="span" sx={styles.boxAdditionalComments}>
-          <FormattedMessage {...messages.additionalComments} />
-        </Box>
-        <Box component="span" sx={styles.boxNote}>
-          <FormattedMessage {...messages.note} />
-        </Box>
-      </Box>
-      <FormControl fullWidth sx={styles.formControlComment}>
-        <TextField multiline rows={6} variant="outlined" sx={styles.textFieldComment} />
-      </FormControl>
       <Link to="/checkout">
         <Button fullWidth variant="contained" size="medium" sx={styles.btnCheckoutNow}>
           <FormattedMessage {...messages.btnCheckoutNow} />
