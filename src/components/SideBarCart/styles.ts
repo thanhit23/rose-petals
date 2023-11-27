3c847b02e1840c0135b38c06ea1e33c620da4191
Pham Van Quan <quan100202@gmail.com>
2023-11-27 17:21:24 +0700

    [Bugfix] cannot close sidebar cart final

--- src/components/SideBarCart/styles.ts
+++ src/components/SideBarCart/styles.ts
@@ -50,17 +50,18 @@ const paperCart = {
   backgroundColor: '#fff',
   boxShadow: 24,
   borderRadius: 'inherit',
-  '& .title-cart-empty': {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+};
+
+const boxTitleCart = {
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+const boxImgEmptyCart = {
+  display: 'flex',
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
 const boxTitlePaper = {
@@ -121,4 +122,6 @@ export default {
   paperCart,
   btnCart,
   badge,
+  boxTitleCart,
+  boxImgEmptyCart,
 };
